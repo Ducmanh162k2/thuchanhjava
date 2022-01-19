36d478af1f55125dc32538474ef746c1bb4ba374
Victorfly98 <bboyquangvinh1998@gmail.com>
2022-01-19 15:36:23 +0700

    update bai thuc hanh 1901

--- BaiThucHanhSo1/src/App.java
+++ BaiThucHanhSo1/src/App.java
@@ -1,5 +1,13 @@
+import java.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, JavaOOP!");
+    public static void main (String[] args) throws Exception {
+        double heSoA, heSoB, nghiemX;
+        System.out.println("Nhap he so A");
+        java.util.Scanner sc = new Scanner(System.in);
+        heSoA = sc.nextDouble();
+        System.out.println("Nhap he so B ");
+        heSoB = sc.nextDouble();
+        System.out.println("He so A la: " + heSoA);
+        
     }
 }
