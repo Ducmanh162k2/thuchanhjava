183d1ca6d96c62d7a673aca6fce1c9811f7eac87
Victorfly98 <bboyquangvinh1998@gmail.com>
2022-03-16 19:56:20 +0700

    nop bài ngay 16/03

---
+++ BaiThucHanh1603/src/App20.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App20 {
+    public static void main(String[] args) throws Exception {
+        HinhTron ht = new HinhTron();
+        HinhTron ht2 = new HinhTron();
+        //
+        System.out.println("Hello, World!");
+    }
+}
---
+++ BaiThucHanh1603/src/App25.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App25 {
+    
+    final float PI = 3.14f;
+
+    float r;
+    float cv;
+    float dt;
+
+    void nhapBanKinh() {
+        System.out.println("Hãy nhập vào bán kính Hình tròn: ");
+        Scanner scanner = new Scanner(System.in);
+        r = scanner.nextFloat();
+    }
+
+    void tinhChuVi() {
+        cv = 2*PI*r;
+    }
+
+    void tinhDienTich() {
+        dt = PI*r*r;
+    }
+
+    void inChuVi() {
+        System.out.println("Chu vi Hình Tròn: "+ cv);
+    }
+
+    void inDienTich() {
+        System.out.println("Dien tich hinh tron: "+ dt);
+    }
+}
